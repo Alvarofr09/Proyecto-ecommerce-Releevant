3748c5352486b7470bd02371800152fa56045712
Alvaro Frias <alvarofriasruiz03@gmail.com>
2023-12-22 12:17:46 +0100

    cambiar el script de prueba al de index

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const container = document.getElementById("container");
+const registroBtn = document.getElementById("registro");
+const loginBtn = document.getElementById("login");
+
+registroBtn.addEventListener("click", () => {
+	container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+	container.classList.remove("active");
+});
